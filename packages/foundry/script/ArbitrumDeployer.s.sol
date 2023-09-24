fb81db3e2eb1328185bc03a0755fff8d2d6f5670
eugenioclrc <eugenioclrc@gmail.com>
2023-09-24 02:03:04 -0300

    Arbutrum deployer

---
+++ packages/foundry/script/ArbitrumDeployer.s.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {Deployer} from "../src/Deployer.sol";
+
+interface Create2Deployer {
+    function safeCreate2(bytes32 salt, bytes calldata initializationCode)
+        external
+        payable
+        returns (address deploymentAddress);
+}
+
+contract CounterScript is Script {
+    address constant CREATE2DEPLOYER = 0x0000000000FFe8B47B3e2130213B802212439497;
+
+    function setUp() public {}
+
+    function run() public {
+        require(msg.sender == 0x0000003FA6D1d52849db6E9EeC9d117FefA2e200, "msg.sender is not the expected");
+        vm.startBroadcast();
+        // (address(bytes20(salt))
+        Create2Deployer(CREATE2DEPLOYER).safeCreate2(
+            //bytes32(bytes20(address(0x0000003FA6D1d52849db6E9EeC9d117FefA2e200))),
+            0x0000003fa6d1d52849db6e9eec9d117fefa2e200000000000000000000000001,
+            abi.encodePacked(type(Deployer).creationCode, abi.encode(msg.sender))
+        );
+        Deployer(0x3A7d8E40Deb0b05fD32Cef705b4865d2041bd575).deploy(false);
+    }
+}
+/*
+https://arbitrum-goerli.publicnode.com	
+
+arbitrum-goerli
+forge script script/MainNft.s.sol --sender 0x0000003FA6D1d52849db6E9EeC9d117FefA2e200 --rpc-url https://arbitrum-goerli.publicnode.com   --broadcast --private-key=PRIVATEKEY --tc CounterScript
+forge verify-contract 0x19FB80CFC5ee42dEafAa4C2FA5BeB5ca91bB6e16 MultichainDeployer --chain=goerli --etherscan-api-key 
+
+https://goerli.arbiscan.io/address/0x3a7d8e40deb0b05fd32cef705b4865d2041bd575
+https://goerli.arbiscan.io/address/0x19FB80CFC5ee42dEafAa4C2FA5BeB5ca91bB6e16
+
+*/
