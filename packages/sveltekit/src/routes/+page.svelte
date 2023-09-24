3a691d66ecac162e4300ddb27b01d9be32f4efad
eugenioclrc <eugenioclrc@gmail.com>
2023-09-24 01:41:56 -0300

    build frontend

--- packages/sveltekit/src/routes/+page.svelte
+++ packages/sveltekit/src/routes/+page.svelte
@@ -1,7 +1,57 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+
+import { web3Modal } from 'svelte-wagmi';
+import { connected, chainId, signerAddress } from 'svelte-wagmi';
+import { keccak256 } from 'viem';
+import { stringToHex } from 'viem'
+import { concat,slice,toBytes } from 'viem'
+
+
+import { toRlp } from 'viem'
+
+
+let saltStr = '';
+$: saltBytes32 = stringToHex( saltStr, { size: 32 });
+
+
+$: senderSalt = ($signerAddress && saltBytes32) ? keccak256(concat([toRlp($signerAddress), toRlp(saltBytes32)])):'';
+
+
+
+// TODO: set factory address
+const FACTORY_ADDR = toBytes('0x1bDf03E0fF5EcC3628Ba54A1630352E7F6067BA8');
+const PROXY_BYTECODE_HASH =toBytes('0x21c35dbe1b344a2488cf3321d6ce542f8e9f305544ff09e4993a62319a497c1fn');
+$: proxy = slice(keccak256(concat([toBytes('0xff'), FACTORY_ADDR, senderSalt, PROXY_BYTECODE_HASH])),12);
+
+$: contract = slice(
+keccak256(
+                        concat([
+                            // 0xd6 = 0xc0 (short RLP prefix) + 0x16 (length of: 0x94 ++ proxy ++ 0x01)
+                            // 0x94 = 0x80 + 0x14 (0x14 = the length of an address, 20 bytes, in hex)
+                            toBytes('0xd694'),
+                            proxy,
+                            toBytes('0x01') // Nonce of the proxy contract (1)
+])), 12);
+
+
+
+/*
+const D694 = 0xd694n
+const _01 = 0x01n
+
+const getRandomContract = (sender) => {
+    const salt = randomBytes(12);
+    const senderSalt = concat([sender, salt]);
+
+    const proxy = keccak('keccak256').update(concat([
+        FF,
+        FACTORY_ADDR,
+        senderSalt,
+        PROXY_BYTECODE_HASH,
+    ]))
+        .digest()
+        .slice(12);
+*/
 </script>
 
 <svelte:head>
@@ -9,51 +59,62 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+<div class="container">
+	<div class="mx-auto max-w-sm">
+	<h1>Mint a contract vanity address</h1>
+	<p>Easy to use!</p>
+	{#if !$connected}
+	<p>
 
-		to your new<br />SvelteKit app
-	</h1>
+		{#if $web3Modal}
+	<button on:click="{() =>$web3Modal.openModal()}"> Connect to Ethereum</button>
+	{:else}
+	Wait for Web3Modal
+	{/if}
+	</p>
+	{:else}
+	
+	<article class="grid">
+        <div>
+          <hgroup>
+            <h1>MINT ADDRESS</h1>
+            <h2>A minimalist demo</h2>
+          </hgroup>
+          <form>
+			<label for="salt">Salt</label>
+            <input
+              type="text"
+			  bind:value={saltStr}
+              name="Salt text"
+              placeholder="Login"
+              aria-label="Login"
+              required
+            />
+			<label for="salt">Salt (bytes32)</label>
+            <input
+              type="text"
+			  readonly
+			  value={saltBytes32}
+              name="Bytes32"
+              placeholder="Bytes32"
+            />
+			<label>Predicted address:</label>
+			<input
+              type="text"
+			  readonly
+			  value={contract}
+              name="address"
+              placeholder="address"
+            />
+            <button type="submit" onclick="event.preventDefault()">MINT</button>
+          </form>
+        </div>
+      </article>
+	  {/if}
+</div>
+  </div>
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
 
-	<Counter />
-</section>
+	
+	
 
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
